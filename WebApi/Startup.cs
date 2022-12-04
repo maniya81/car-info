c98b9b656b1f02a79f61fff8d94fc33d5c4f7121
Sagar Maniya <ManiyaSagar26@gmail.com>
2022-12-04 20:20:00 +0530

    swagger config update

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -50,12 +50,15 @@ namespace CarInfo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CarInfo v1"));
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CarInfo v1"));
+            //}
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CarInfo v1"));
 
             //app.UseHttpsRedirection();
 
