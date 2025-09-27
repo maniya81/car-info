2bd473e4ec27e3a605d781338a4abba9bc23b727
Sagar Maniya <maniyasagar26@gmail.com>
2025-09-27 22:59:18 +1000

    Menu Change and home minor changes

--- ClientApp/src/app/component/menu/menu.component.ts
+++ ClientApp/src/app/component/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subject, timer } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-menu',
@@ -7,8 +9,38 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
   isCollapsed = true;
+  hovered = false;
+
+  private toggleCollapse$ = new Subject<void>();
+  private menuLinkClick$ = new Subject<void>();
+
+  constructor() {
+    // Handle collapse toggle and hover effect
+    this.toggleCollapse$
+      .pipe(
+        tap(() => {
+          this.isCollapsed = !this.isCollapsed;
+          this.hovered = true;
+        }),
+        switchMap(() => timer(1000)),
+        tap(() => (this.hovered = false))
+      )
+      .subscribe();
+
+    // Handle menu link click and collapse after delay
+    this.menuLinkClick$
+      .pipe(
+        switchMap(() => timer(500)),
+        tap(() => (this.isCollapsed = true))
+      )
+      .subscribe();
+  }
 
   toggleCollapse() {
-    this.isCollapsed = !this.isCollapsed;
+    this.toggleCollapse$.next();
+  }
+
+  onMenuLinkClick() {
+    this.menuLinkClick$.next();
   }
 }
